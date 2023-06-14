146995ade2f2c92d2453c14e6ca6229f2a0a8aaa
zacariasjuliano <zacariasjuliano@gmail.com>
2023-06-14 14:42:17 +0100

    Atualização de validação quando a nota for negativa.

--- ruby/exec.rb
+++ ruby/exec.rb
@@ -2,38 +2,32 @@
 print("Insira o nome do aluno: ")
 aluno_nome = gets
 
-print("Insira a nota de Matemática: ")
-nota_matematica = gets.chomp.to_f 
-#Verifica se a nota de matématica 
-if nota_matematica < 0
-    print("A nota de matemática não pode ser negativa: ")
+begin
+    print("Insira a nota de matemática: ")
     nota_matematica = gets.chomp.to_f 
-end
+#Verifica se a nota de matématica é positiva
+end while nota_matematica < 0
 
-print("Insira a nota de Português: ")
-nota_portugues = gets.chomp.to_f 
-#Verifica se a nota de português
-if nota_portugues < 0
-    print("A nota de português não pode ser negativa: ")
+begin
+    print("Insira a nota de português: ")
     nota_portugues = gets.chomp.to_f 
-end
+#Verifica se a nota de português é positiva
+end while nota_portugues < 0
 
-print("Insira a nota de Ciências: ")
-nota_ciencias = gets.chomp.to_f
-#Verifica se a nota de ciências
-if nota_portugues < 0
-    print("A nota de ciências não pode ser negativa: ")
+begin
+    print("Insira a nota de ciências: ")
     nota_ciencias = gets.chomp.to_f 
-end 
+#Verifica se a nota de ciências é positiva
+end while nota_ciencias < 0
 
 nota_media = (nota_matematica + nota_portugues + nota_ciencias) / 3
 
 print("------------------------------------------------------\n")
 puts("Nome: #{aluno_nome}")
 puts("Notas:")
-puts("Matématica: #{nota_matematica}")
-puts("Português: #{nota_portugues}")
-puts("Ciências: #{nota_ciencias}")
+puts(" Matématica: #{nota_matematica}")
+puts(" Português: #{nota_portugues}")
+puts(" Ciências: #{nota_ciencias}")
 puts("Média: #{nota_media}")
 print("------------------------------------------------------\n")
 
