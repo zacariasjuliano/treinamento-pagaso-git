a920d23a83bc5b77c3de2e48dcefee1ed4cf2ef2
zacariasjuliano <zacariasjuliano@gmail.com>
2023-06-15 17:41:37 +0100

    Criou-se funções.

--- ruby/calculadora.rb
+++ ruby/calculadora.rb
@@ -1,3 +1,29 @@
+#Declaração de funções
+def operacao (primeiro, operador, segundo)
+    #Verifiva a operação a ser efetuada
+    case operador
+    when "+"
+        resultado = primeiro + segundo
+        puts("Resultado: #{resultado}")
+    when "/"
+        if segundo_numero == 0
+            print("A operação não pode ser realizada, porque o dividendo é zero.\n")
+        else
+            resultado = primeiro / segundo
+            puts("Resultado: #{resultado}")
+        end
+    when "-"
+        resultado = primeiro - segundo
+        puts("Resultado: #{resultado}")
+    when "*"
+        resultado = primeiro * segundo
+        puts("Resultado: #{resultado}")
+    else
+        puts("Operador escolhido é inválido.")
+    end
+end
+
+
 print("CALCULADORA BASICA - RUBY\n")
 
 print("Insira o primeiro número: ")
@@ -14,25 +40,5 @@ operador = gets.chomp.to_s
 print("Insira o segundo número: ")
 segundo_numero = gets.chomp.to_f
 
-#Verifiva a operação a ser efetuada
-case operador
-when "+"
-    resultado = primeiro_numero + segundo_numero
-    puts("Resultado: #{resultado}")
-when "/"
-    if segundo_numero == 0
-        print("A operação não pode ser realizada, porque o dividendo é zero.\n")
-    else
-        resultado = primeiro_numero / segundo_numero
-        puts("Resultado: #{resultado}")
-    end
-when "-"
-    resultado = primeiro_numero - segundo_numero
-    puts("Resultado: #{resultado}")
-when "*"
-    resultado = primeiro_numero * segundo_numero
-    puts("Resultado: #{resultado}")
-else
-    puts("Operador escolhido é inválido.")
-end
+operacao(primeiro_numero, operador, segundo_numero)
 
--- ruby/exec.rb
+++ ruby/exec.rb
@@ -2,23 +2,34 @@
 print("Insira o nome do aluno: ")
 aluno_nome = gets
 
-begin
-    print("Insira a nota de matemática: ")
-    nota_matematica = gets.to_f 
 #Verifica se a nota de matématica é positiva ou maior que 20
-end while nota_matematica < 0 || nota_matematica > 20
+def verifica_nota (disciplina)
+    begin
+        print("Insira a nota de #{disciplina}: ")
+        nota = gets.to_f 
+    end while nota < 0 || nota > 20
+        
+    return nota
+end
 
-begin
-    print("Insira a nota de português: ")
-    nota_portugues = gets.to_f 
-#Verifica se a nota de português é positiva ou maior que 20
-end while nota_portugues < 0 || nota_portugues > 20
+#Verifica a situação do estudante
+def verifica_situacao (media)
+    if media < 4
+        print("Situação: Reprovado.\n")
+    elsif media >= 5 && media < 6.9
+        print("Situação: Recuperação.\n")
+    else
+        if media >= 6.9
+            print("Situação: Aprovado.\n")
+        else
+            print("Situação: \n")
+        end
+    end
+end
 
-begin
-    print("Insira a nota de ciências: ")
-    nota_ciencias = gets.to_f 
-#Verifica se a nota de ciências é positiva ou maior que 20
-end while nota_ciencias < 0 || nota_ciencias > 20
+nota_matematica = verifica_nota("matemática")
+nota_portugues = verifica_nota("português")
+nota_ciencias = verifica_nota("ciências")
 
 nota_media = (nota_matematica + nota_portugues + nota_ciencias) / 3
 
@@ -31,15 +42,6 @@ puts(" Ciências: #{nota_ciencias}")
 puts("Média: #{nota_media}")
 print("------------------------------------------------------\n")
 
-if nota_media < 4
-    print("Situação: Reprovado.\n")
-elsif nota_media >= 5 && nota_media < 6.9
-    print("Situação: Recuperação.\n")
-else
-    if nota_media >= 6.9
-        print("Situação: Aprovado.\n")
-    else
-        print("Situação: \n")
-    end
-end
+verifica_situacao(nota_media)
+
 
