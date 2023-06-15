57d169048461749bf6798a820a61cb30247bd9a9
zacariasjuliano <zacariasjuliano@gmail.com>
2023-06-15 16:28:55 +0100

    Melhorias nas condições

--- ruby/exec.rb
+++ ruby/exec.rb
@@ -4,21 +4,21 @@ aluno_nome = gets
 
 begin
     print("Insira a nota de matemática: ")
-    nota_matematica = gets.chomp.to_f 
-#Verifica se a nota de matématica é positiva
-end while nota_matematica < 0
+    nota_matematica = gets.to_f 
+#Verifica se a nota de matématica é positiva ou maior que 20
+end while nota_matematica < 0 || nota_matematica > 20
 
 begin
     print("Insira a nota de português: ")
-    nota_portugues = gets.chomp.to_f 
-#Verifica se a nota de português é positiva
-end while nota_portugues < 0
+    nota_portugues = gets.to_f 
+#Verifica se a nota de português é positiva ou maior que 20
+end while nota_portugues < 0 || nota_portugues > 20
 
 begin
     print("Insira a nota de ciências: ")
-    nota_ciencias = gets.chomp.to_f 
-#Verifica se a nota de ciências é positiva
-end while nota_ciencias < 0
+    nota_ciencias = gets.to_f 
+#Verifica se a nota de ciências é positiva ou maior que 20
+end while nota_ciencias < 0 || nota_ciencias > 20
 
 nota_media = (nota_matematica + nota_portugues + nota_ciencias) / 3
 
@@ -33,9 +33,13 @@ print("------------------------------------------------------\n")
 
 if nota_media < 4
     print("Situação: Reprovado.\n")
-elsif nota_media >= 5 && nota_media < 6.5
+elsif nota_media >= 5 && nota_media < 6.9
     print("Situação: Recuperação.\n")
 else
-    print("Situação: Aprovado.\n")
+    if nota_media >= 6.9
+        print("Situação: Aprovado.\n")
+    else
+        print("Situação: \n")
+    end
 end
 
